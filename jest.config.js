066ee5df2f8268db7b94b67d71ee8d92c8c3828e
Juan Medina <jmedinaguerrero847@gmail.com>
2025-01-09 11:24:50 -0500

    Refactor Jest configuration and add setup file for unit tests

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+};
---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+  removeItem: jest.fn(),
+};
+global.localStorage = localStorageMock;
